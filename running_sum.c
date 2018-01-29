98e724778e3beb43d6e2482dc7b23a3e8a1cfc04
Bobby Pearson <robert.pearson@sky.uk>
2018-01-29 16:22:56 +0000

    running_sum example from 1.6

---
+++ running_sum.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    int     i;
+    double  x, min, max, sum, avg;
+
+    if (scanf("%lf", &x) != 1) {
+        printf("No data - bye!");
+        exit(1);
+    }
+
+    min = max = sum = avg = x;
+
+    printf("%5s%9s%9s%9s%12s%12s\n%5s%9s%9s%9s%12s%12s\n\n",
+           "Count", "Item", "Min", "Max", "Sum", "Average",
+           "-----", "----", "---", "---", "---", "-------");
+
+    printf("%5d%9.1f%9.1f%9.1f%12.3f%12.3f\n",
+           1, x, min, max, sum, avg);
+
+    for(i = 2; scanf("%lf", &x) == 1; ++i) {
+
+        if (x < min)
+            min = x;
+        else if (x > min)
+            max = x;
+
+        sum += x;
+        avg = sum / i;
+
+        printf("%5d%9.1f%9.1f%9.1f%12.3f%12.3f\n",
+               i, x, min, max, sum, avg);
+    }
+}
