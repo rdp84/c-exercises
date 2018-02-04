c910a714f4f1942b1433b2305da05f619c5e7d32
Bobby Pearson <pearson.bobby@gmail.com>
2018-02-04 12:50:18 +0000

    Further examples from Chapter 1

---
+++ abc.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXSTRING 100
+
+int main (void)
+{
+  char c = 'a', *p, s[MAXSTRING];
+
+  p = &c;
+  printf("%c%c%c  ", *p, *p + 1, *p + 2);
+  strcpy(s, "ABC");
+  printf("%s  %c%c%s\n", s, *s + 6, *s + 7, s + 2);
+  strcpy(s, "She sells sea shells on the seashore");
+  p = s + 14;
+  for ( ; *p != '\0'; ++p) {
+    if (*p == 'e')
+      *p = 'E';
+    if (*p == ' ')
+      *p = '\n';
+  }
+
+  printf("%s\n", s);
+  return 0;
+}
