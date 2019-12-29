2901428582f805e8750d84c14282493ad21b4000
Bobby Pearson <pearson.bobby@gmail.com>
2019-12-29 18:59:01 +0000

    Added some comments

--- chapter_6/exercises/number_1.c
+++ chapter_6/exercises/number_1.c
@@ -2,19 +2,23 @@
 
 int main(void)
 {
-  /*
-  int a[2], *p, *q;
 
-  p = a;
-  q = p + 1;
-  printf("%li\n", q - p);
-  printf("%li\n", (long int) q - (long int) p);
-  */
+  double a[2], *d1, *d2;
+
+  d1 = a;
+  d2 = d1 + 1;
+  printf("%li\n", d2 - d1); /* 1 is printed */
+  printf("%li\n", (long) d2 - (long) d1); /* 8 is printed */
 
   char *format = "%p %d %d %d\n";
   int i = 3;
   int *p = &i;
 
-  printf("%s%li\n", "Value of p: ", (p - (p - 2)));
+  printf("%s%p\n", "Value of p: ", p);
+  printf("%s%p\n", "Value of &p: ", &p);
+  printf("%s%p\n", "Value of *&p: ", *&p);
+  printf("%s%p\n", "Value of p - 2: ", p - 2); /* int is 4 bytes therefore prints an address 8 bytes less than p */
+  printf("%s%li\n", "Value of (p - (p - 2)): ", (p - (p - 2))); /* 2 is printed because address of (p - 2) is 2 integers less than p */
+
   printf(format, p, *p + 7, 3 * **&p + 1, 5 * (p - (p - 2)));
 }
