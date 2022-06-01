c02fa3e60eaea0a0fa74e7a9a7616bfd9fc3f802
Bobby Pearson <robert.pearson@sky.uk>
2022-06-01 21:47:48 +0100

    strcmp example

--- kernighan_ritchie/chapter_5/char_ptrs_fcts/strcmp.c
+++ kernighan_ritchie/chapter_5/char_ptrs_fcts/strcmp.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 
 int strcmp_arrays(char *, char *);
+int strcmp_ptrs_1(char *, char *);
+int strcmp_ptrs_2(char *, char *);
 
 int
 main(void)
 {
-  char *s = "Hello, World";
-  char *t = "Hello, World";
+  char *s = "Hello, WOrld";
+  char *t = "HeLlo, World";
 
-  printf("s: %s\nt: %s\nstrcmp(s, t): %d\n", s, t, strcmp_arrays(s, t));
+  printf("s: %s\nt: %s\nstrcmp_arrays(s, t): %d\n", s, t, strcmp_arrays(s, t));
+  printf("s: %s\nt: %s\nstrcmp_ptrs_1(s, t): %d\n", s, t, strcmp_ptrs_1(s, t));
+  printf("s: %s\nt: %s\nstrcmp_ptrs_2(s, t): %d\n", s, t, strcmp_ptrs_2(s, t));
   
   return EXIT_SUCCESS;
 }
 
-/* strcmp: return <0 if s < t, 0 if s == t, >0 if s > t  */
+/* strcmp: return <0 if s < t, 0 if s == t, >0 if s > t; array subscript version */
 int
 strcmp_arrays(char *s, char *t)
 {
@@ -24,4 +28,27 @@ strcmp_arrays(char *s, char *t)
     if (s[i] == '\0')
       return 0;
   return s[i] - t[i];
+}
+
+/* strcmp: return <0 if s < t, 0 if s == t, >0 if s > t; pointer version 1 */
+int
+strcmp_ptrs_1(char *s, char *t)
+{
+
+  for(; *s == *t; s++, t++)
+    if (*s == '\0')
+      return 0;
+  return *s - *t;
+}
+
+/* strcmp: return <0 if s < t, 0 if s == t, >0 if s > t; pointer version 2 */
+int
+strcmp_ptrs_2(char *s, char *t)
+{
+  while (*s == *t) {
+    if (*s == '\0')
+      return 0;
+    s++, t++;
+  }
+  return *s - *t;
 }
